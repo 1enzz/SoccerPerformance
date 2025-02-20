f222b0527436b2248eb1d71d684bc486c022a3aa
Enzo Oliveira <enzo.oliveira@sislogica.com.br>
2025-02-20 10:53:23 -0300

    Adição do footer; Adição de funcionalidades de scroll onclick;

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function scrollSobre(){
+    $('html, body').animate({ scrollTop: $('.about-title').offset().top }, 'slow');
+}
+function scrollTeam(){
+    $('html, body').animate({ scrollTop: $('.team-title').offset().top }, 'slow');
+}
